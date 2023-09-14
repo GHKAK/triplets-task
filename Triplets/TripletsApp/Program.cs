05666b7f37d8793674827e387bc4bf525b7b8e54
GHKAK <87258919+GHKAK@users.noreply.github.com>
2023-09-15 02:36:42 +0300

    algorithm scheme

---
+++ Triplets/TripletsApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using Triplets;
+
+int wordsCount = 100000;
+string projectFolderPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+string filePath = Path.Combine(projectFolderPath, "text.txt");
+if (!File.Exists(filePath)) {
+    new TextGenerator().GenerateRandomText(filePath, wordsCount);
+}
+await new TripletsCounter().CountTriplets(filePath);
---
+++ Triplets/TripletsApp/TextGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Xml.Serialization;
+
+namespace Triplets; 
+
+public class TextGenerator {
+    private Random _random = new Random();
+
+    private string[] _words = {
+        "I'm", "skill", "plus", "minus", "man", "algorithm", "computer", "character", "general", "form", "performing",
+        "woman", "our", "logarithm", "aaabbbb", "bbbaa", "cccaabbaacc"
+    };
+    string[] _punctuation = { ".", ",", "!", "?", ";", ":","\n", "\t" };
+
+    public void GenerateRandomText(string filePath, int wordsCount) {
+        using (StreamWriter writer = new StreamWriter(filePath)) {
+            for (int i = 0; i < wordsCount; i++) {
+                writer.Write(_words[_random.Next(_words.Length)]);
+                writer.Write(_punctuation[_random.Next(_punctuation.Length)]);
+                writer.Write(" ");
+            }
+        }
+    }
+}
